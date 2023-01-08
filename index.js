8ccc47b05be31c8d5cfed2099873aaacae40ca22
koker1 <88758670+koker1@users.noreply.github.com>
2023-01-08 18:40:14 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+// first code on js
+// console.log('Hello World'); 
+
+//let name = 'koker';
+//console.log(name);
+
+//let firstName = 'Loh';
+//let lastName = 'Kok Hao';
+
+//console.log(firstName,lastName);
+
+//let interestRate = 0.3;
+//interestRate = 1;
+//console.log(interestRate);
+
+//let name = 'koker'; // string literal
+//let age = 30; // number literal
+//let isMale = true; // boolean literal
+//let firstName = undefined;
+//let lastName = null;
+
+//console.log('hi, my name is',name);
+//console.log('im',age);
+//console.log('am i male?',isMale);
+
+//let name = 'koker'; 
+//let age = 30; 
+//let person = {
+//    name: 'koker',
+//    age: 30
+//};
+
+//dot notation
+//person.name = 'bro';
+//person.age = 69;
+
+//bracket notation
+//let selection = 'name';
+//person[selection] = 'tate';
+//person['name'] = 'tate';
+//console.log(person);
+
+//arrays
+//let selectedColors = ['red','blue'];
+//selectedColors[2] = 'green';
+//console.log(selectedColors[0]);
+//console.log(selectedColors);
+//console.log(selectedColors.length);
+
+//functions
+//perfroming a task
+function greet(name, lastName){
+    console.log('Hello World',name,'',lastName);
+}
+
+greet('koker');
+greet('bro');
+greet('koker','loh');
+
+//calculating a value
+function square(number){
+    return number*number;
+}
+//let number = square(4);
+//console.log(number);
+console.log(square(2));
